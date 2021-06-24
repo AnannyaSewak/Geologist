325112b7566ca0cefe7b0a31c468e24eb05a859a
AnannyaSewak <76039979+AnannyaSewak@users.noreply.github.com>
2021-06-24 19:48:18 +0530

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -20,8 +20,8 @@ class Stone{
         translate(pos.x, pos.y);
         rotate(angle);
         strokeWeight(3);
-        stroke('white')
-        fill('orange')
+        stroke('black')
+        fill('red')
         rectMode(CENTER)
         rect(0, 0, this.width, this.height);
         pop();
